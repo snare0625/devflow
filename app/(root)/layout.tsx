9bca689a4519ca9b7de40795a3c55daf89336686
Neo Sekhukhune <sekhukhune056@gmail.com>
2026-05-05 12:22:51 +0200

    Implemented SideBar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,18 @@
 import Navbar from "@/components/navigation/navbar";
 import { ReactNode } from "react";
+import LeftSidebar from "@/components/navigation/navbar/LeftSidebar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
+    <main className="background-light850_dark100 relative">
       <Navbar />
-      {children}
+      <div className="flex">
+        <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 px-6 pt-36 pb-6 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+      </div>
     </main>
   );
 };
